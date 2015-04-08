891475d75e5fb8b32632c19f01b7cb851a194346
hiukim <hiukim@gmail.com>
2015-04-09 03:41:25 +0800

    Refactor

--- server/publishes.es6.js
+++ server/publishes.es6.js
@@ -10,9 +10,7 @@ Meteor.publish('customers', function() {
   if (Users.isAdmin(this.userId)) {
     return Users.findCustomers();
   } else if (Users.isAssistant(this.userId)) {
-    var placements = Placements.find({assistantId: this.userId}, {fields: {customerId: 1}});
-    var customerIds = _.pluck(placements.fetch(), 'customerId');
-    return Users.findCustomers({_id: {$in: customerIds}});
+    return Users.findCustomers();
   } else {
     return [];
   }
