fec349db3a240de7120cac78370401cca928e806
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-04-09 16:18:55 +0800

    Added as comments

--- client/templates/customers/header.es6.js
+++ client/templates/customers/header.es6.js
@@ -1,20 +1,32 @@
+var _toggleTwoClass = function(bool, onClass, offClass) {
+
+  return bool ? onClass : offClass;
+}
+
 Template.customerHeader.helpers({
   numberOfTasks : function numberOfTasks() {
     //TODO integration
     return this.tasks ? this.tasks.length : 0;
   },
-  showCallAlertComponent : function() {
+  isCalling : function() {
     //TODO change to customer.isCalling when integrated
-    return Session.get("isCalling") ? "callAlert" : null;
+    return _toggleTwoClass(Session.get("isCalling"), "on", "off");
+  },
+
+  isCallingShowInvisible : function() {
+    //TODO change to customer.isCalling when integrated
+    return _toggleTwoClass(Session.get("isCalling"), "show pulse infinite", "invisible");
   }
 });
 
 Template.customerHeader.events({
   "click .call-me" : function() {
     //TODO change to customer.isCalling when integrated
-    Session.set("isCalling", true);
+    Session.set("isCalling", !Session.get("isCalling"));
+
   }
 });
+
 
 //TODO remove when customer.isCalling is finished
 Template.customerHeader.destroyed = function() {
