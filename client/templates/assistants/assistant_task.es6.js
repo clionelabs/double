897b1afa7ac1b5fe7f8c451b43a758648f48969e
hiukim <hiukim@gmail.com>
2015-04-18 12:08:03 +0800

    Tasks

--- client/templates/assistants/assistant_task.es6.js
+++ client/templates/assistants/assistant_task.es6.js
@@ -2,6 +2,15 @@ Template.assistantTask.helpers({
 
 });
 
+Template.assistantTask.events({
+  "click button.start": function() {
+    Tasks.startWork(this._id);
+  },
+  "click button.end": function() {
+    Tasks.endWork(this._id);
+  }
+});
+
 Template.assistantTaskSubItem.helpers({
   assistantTaskSubItemMessage : function() {
     return this.url ? "assistantTaskSubItemMessageWithUrl" : "assistantTaskSubItemMessageWithoutUrl" ;
