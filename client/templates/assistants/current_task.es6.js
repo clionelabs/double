647ab0f7e75419a042079fbf9b0a172aad121a1d
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-04-23 18:09:31 +0800

    Added timer function

--- client/templates/assistants/current_task.es6.js
+++ client/templates/assistants/current_task.es6.js
@@ -1,5 +1,30 @@
+Template.currentTask._timeoutFn = null;
+
 Template.currentTask.onCreated(function() {
   this.data = _.extend(
       this.data,
       { isCurrent : true });
+});
+
+Template.currentTask.onRendered(function() {
+  Template.currentTask._timeoutFn = Meteor.setInterval(function() {
+    let currentTask = Template.assistantDashboardCustomerTab.getCurrentTask();
+    Session.setAuth(SessionKeys.currentTimeUsed, currentTask.getCurrentSectionDuration());
+  }, 1000);
+
+});
+
+Template.currentTask.helpers({
+  getTimeUsed : function() {
+    return moment.duration(+Session.get(SessionKeys.currentTimeUsed)).format('hh:mm:ss', { trim : false });
+  },
+  getCustomerName : function() {
+    let currentTask = Template.assistantDashboardCustomerTab.getCurrentTask();
+    return Users.findCustomers({ _id : currentTask.requestorId }).fetch()[0].displayName();
+  }
+});
+
+Template.currentTask.onDestroyed(function(){
+  Meteor.clearInterval(Template.currentTask._timeoutFn);
+  Template.currentTask._timeoutFn = null;
 });
