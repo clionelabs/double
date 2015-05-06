2c792876261a4d1e7ad4d39adf77d0749baa4224
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-05-06 22:43:29 +0800

    Added user preferences functionality

--- client/templates/assistants/assistant_customer_tab.es6.js
+++ client/templates/assistants/assistant_customer_tab.es6.js
@@ -49,3 +49,42 @@ Template.assistantDashboardCustomerTab.events({
     Modal.show('assistantCreateTaskSchedule', data);
   }
 });
+
+Template.assistantDashboardCustomerTab._submitPreferenceFn = (form, taskId, isCurrent) => {
+  let message = form.target.message.value;
+  Tasks.Status.change(message, taskId,
+      () => {
+        form.target.reset();
+        Session.setAuth(SessionKeys.genStatusFormKey(taskId, isCurrent), false);
+      });
+};
+
+Template.assistantTaskStatusForm.onRendered(function() {
+  let selfTemplate = this;
+  selfTemplate.$('.form-container').on('transitionend onanimationend', function(e) {
+    if ($(e.target).height() > 1) {
+      selfTemplate.$('.status-message').focus();
+    }
+  });
+});
+
+Template.assistantTaskStatusForm.helpers({
+  isStatusFormShown : function() {
+    return Session.get(SessionKeys.genStatusFormKey(this._id, this.isCurrent)) ? "" : "not-shown";
+  },
+  genFormKey : function(taskId, isCurrent) {
+    return SessionKeys.genStatusFormKey(taskId, isCurrent);
+  }
+});
+
+Template.assistantTaskStatusForm.events({
+  "submit .task-status-change" : function(e) {
+    e.preventDefault();
+    return Template.assistantTaskStatusForm._submitFn(e, this._id, this.isCurrent);
+  },
+  "keyup .task-status-change input" : function(e) {
+    if (e.keyCode === 27) {//esc
+      Session.setAuth(SessionKeys.genStatusFormKey(this._id, this.isCurrent), false);
+    }
+  }
+});
