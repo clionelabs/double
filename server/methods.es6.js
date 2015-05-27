4fabcf093139e64b8adf8fcbbd9db08a88d61123
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-05-27 22:41:05 +0800

    Added meteor method

--- server/methods.es6.js
+++ server/methods.es6.js
@@ -32,7 +32,7 @@ Meteor.methods({
       profile: {
         firstname: firstname,
         lastname: lastname,
-        plan : plan
+        plan: plan
       }
     });
 
@@ -47,13 +47,13 @@ Meteor.methods({
     let plan = data.plan;
 
     Users.editCustomer(userId,
-    {
-      profile: {
-        firstname: firstname,
-        lastname: lastname,
-        plan : plan
-      }
-    });
+        {
+          profile: {
+            firstname: firstname,
+            lastname: lastname,
+            plan: plan
+          }
+        });
   },
 
   requestLoginAccess(email) {
@@ -71,5 +71,33 @@ Meteor.methods({
     }
     let user = Meteor.users.findOne(oldLink.userId);
     LoginLinks.createAndSendAccess(user);
+  },
+
+  exportTimesheet(from, to, customerId) {
+    let customer = Users.findOne({ _id : customerId});
+    let tasks = customerId ? Tasks.findRequestedBy(customerId).fetch() : Tasks.find().fetch();
+    let data = _.reduce(tasks, (memo, task) => {
+      let base = {
+        customerId : customer._id,
+        customerName : customer.displayName(),
+        taskId: task._id,
+        taskTitle: task.title
+      };
+      return memo.concat(_.reduce(task.timesheets, (memo2, timesheet) => {
+        if (timesheet.isWithin(from, to)) {
+          let timeSheetToBeExport = {
+            from: moment(timesheet.startedAt).format("YYYY-MM-DD HH:mm:ss"),
+            to: moment(timesheet.endedAt).format("YYYY-MM-DD HH:mm:ss"),
+            duration: moment.duration(timesheet.duration()).format('HH [hr] mm [min] ss [sec]')
+          };
+          return memo2.concat([_.extend(base, timeSheetToBeExport)]);
+        } else {
+          return memo2;
+        }
+      }, []));
+    }, []);
+
+    return CSV.export(data);
   }
+
 });
