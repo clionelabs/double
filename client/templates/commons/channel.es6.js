f46f21ff1731c435b3e6b8b75f72845cea38ffeb
hiukim <hiukim@gmail.com>
2015-06-02 15:39:53 +0800

    slack integration
      - unrouted channels tab
      - assign channel to customer
      - moved Users into double.core (D.Users) for sharing access control over multiple
        apps

---
+++ client/templates/commons/channel.es6.js
@@ -0,0 +1,17 @@
+Template.channel.events({
+  "click .assign-customer": function(event) {
+    let customerId = this._id;
+    let channelId = this.channelId;
+    D.Channels.assignChannelToCustomer(channelId, customerId);
+  }
+});
+
+Template.channelSlack.helpers({
+  availableCustomers() {
+    let channelId = this._id;
+    let data = Router.current().data();
+    return data.customers.map(function(customer) {
+      return _.extend(customer, { channelId: channelId }, User);
+    });
+  }
+});
