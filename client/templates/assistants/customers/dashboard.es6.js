237fcda905c68eb288f4717af1360c80b77d2a18
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-06-02 15:47:24 +0800

    Added new task button

--- client/templates/assistants/customers/dashboard.es6.js
+++ client/templates/assistants/customers/dashboard.es6.js
@@ -3,3 +3,13 @@ Template.assistantCustomersDashboard.helpers({
     return _.extend(Session.get(SessionKeys.CURRENT_CUSTOMER), Customer, User);
   }
 });
+
+Template.assistantCustomersDashboard.events({
+  "click .new-task-button": function() {
+    let currentCustomer = Template.assistantCustomersDashboard.__helpers.get('getCurrentCustomer')();
+    let data = {
+      customerId: currentCustomer._id
+    };
+    Modal.show('assistantCreateTask', data);
+  }
+});
