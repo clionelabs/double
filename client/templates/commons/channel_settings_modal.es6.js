3f4210e83151a2e06ae1b7eee5774be072549052
hiukim <hiukim@gmail.com>
2015-06-04 13:39:08 +0800

    refactor

--- client/templates/commons/channel_settings_modal.es6.js
+++ client/templates/commons/channel_settings_modal.es6.js
@@ -2,27 +2,30 @@ Template.channelSettingsModal.helpers({
   availableCustomers() {
     let channelId = this._id;
     let data = Router.current().data();
-    return data.customers.map(function(customer) {
+    return data.customers.map((customer) => {
       return _.extend(customer, { channelId: channelId }, User);
     });
   }
 });
 
 Template.channelSettingsModal.events({
-  "click .assign-customer": function(event) {
-    let customerId = this._id;
-    let channelId = this.channelId;
-    D.Channels.assignChannelToCustomer(channelId, customerId);
-    Modal.hide();
-  },
-  "click .assign-none": function(event) {
+  "click .assign-none": function() {
     let channelId = this._id;
     D.Channels.unassignChannel(channelId);
     Modal.hide();
   },
-  "click .assign-spam": function(event) {
+  "click .assign-spam": function() {
     let channelId = this._id;
     D.Channels.assignChannelToSpam(channelId);
+    Modal.hide();
+  }
+});
+
+Template.channelSettingsModalAssignCustomer.events({
+  "click .assign-customer": function() {
+    let customerId = this._id;
+    let channelId = this.channelId;
+    D.Channels.assignChannelToCustomer(channelId, customerId);
     Modal.hide();
   }
 });
