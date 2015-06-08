9f26abe8a14928e9ec678e6ec50a62108e44cafe
hiukim <hiukim@gmail.com>
2015-06-08 23:26:54 +0800

    conversation - auto scroll to bottom

--- client/templates/commons/channel.es6.js
+++ client/templates/commons/channel.es6.js
@@ -1,3 +1,14 @@
+Template.channelMessage.onRendered(function() {
+  let $messageDiv = this.$(".message").parent();
+  $messageDiv.scrollTop($messageDiv.prop("scrollHeight"));
+});
+
+Template.channelMessages.helpers({
+  messages() {
+    return this.messages({sort: {timestamp: 1}});
+  }
+});
+
 Template.channelTitle.helpers({
   templateName() {
     let suffix = s(this.category.toLowerCase()).capitalize().value();
