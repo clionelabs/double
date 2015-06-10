a37faef4b784912ba33684b8ce8fa3c0e6825526
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-06-10 16:36:42 +0800

    Removed partial

--- client/templates/assistant/customers/invoice.es6.js
+++ client/templates/assistant/customers/invoice.es6.js
@@ -15,41 +15,42 @@ Template.invoice.helpers({
 
 Template.taskInvoiceItem.helpers({
   getTimeSheets() {
-    let reduceTimeSheet = (startedAt, endedAt, statuses, memo, timesheetsOfUser, userId) => {
-      let attachOwnerIdAndStatusesToTimesheet = (statuses, timesheet, index) => {
-        let statusesAfter = startedAt;
-        if (index !== 0) {
-          statusesAfter = timesheet.startedAt;
-        }
-
-        let statusesBefore = endedAt;
-        if (timesheetsOfUser[index+1]) {
-          statusesBefore = timesheetsOfUser[index+1].startedAt;
-        }
-
-        let statusesInRange = _.filter(statuses[userId], (status) => {
-          return status.createdAt >= statusesAfter && status.createdAt < statusesBefore;
-        });
-
-        return _.extend({ userId: userId, statuses : statusesInRange }, timesheet);
-      };
-
-      let timesheetsWithOwnerIdAndStatuses
-          = _.map(timesheetsOfUser, _.partial(attachOwnerIdAndStatusesToTimesheet, statuses));
-      return memo.concat(timesheetsWithOwnerIdAndStatuses);
-    };
-
-    let filterTimesheet = (from, to, timesheetsOfUser, ownerId, timesheets) => {
-      timesheets[ownerId] = _.filter(timesheetsOfUser, (timesheet) => {
-        return timesheet.startedAt >= from && timesheet.startedAt < to;
-      });
-    };
-
+    let from = this.queryRange.from;
+    let to = this.queryRange.to;
     let timesheets = _.clone(this.timesheets);
-    _.each(timesheets, _.partial(filterTimesheet, this.queryRange.from, this.queryRange.to));
+    _.each(timesheets,
+        (timesheetsOfUser, ownerId, timesheets) => {
+          timesheets[ownerId] = _.filter(timesheetsOfUser, (timesheet) => {
+            return timesheet.startedAt >= from && timesheet.startedAt < to;
+          });
+        });
+    let statuses = this.statuses;
 
     return _.reduce(timesheets,
-        _.partial(reduceTimeSheet, this.queryRange.from, this.queryRange.to, this.statuses), []);
+        (memo, timesheetsOfUser, userId) => {
+
+          let timesheetsWithOwnerIdAndStatuses
+              = _.map(timesheetsOfUser,
+                    (timesheet, index) => {
+                      let statusesAfter = from;
+                      if (index !== 0) {
+                        statusesAfter = timesheet.startedAt;
+                      }
+
+                      let statusesBefore = to;
+                      if (timesheetsOfUser[index+1]) {
+                        statusesBefore = timesheetsOfUser[index+1].startedAt;
+                      }
+
+                      let statusesInRange = _.filter(statuses[userId], (status) => {
+                        return status.createdAt >= statusesAfter && status.createdAt < statusesBefore;
+                      });
+
+                      return _.extend({ userId: userId, statuses : statusesInRange }, timesheet);
+                    });
+          return memo.concat(timesheetsWithOwnerIdAndStatuses);
+        },
+        []);
   }
 
 });
