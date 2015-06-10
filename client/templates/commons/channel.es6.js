53c770724a5e76b448bc6b5b9783ac05630467b0
hiukim <hiukim@gmail.com>
2015-06-10 15:47:05 +0800

    slack reply message

--- client/templates/commons/channel.es6.js
+++ client/templates/commons/channel.es6.js
@@ -3,6 +3,23 @@ Template.channelMessage.onRendered(function() {
   $messageDiv.scrollTop($messageDiv.prop("scrollHeight"));
 });
 
+Template.channelMessage.helpers({
+  inOutClass() {
+    switch (this.inOut) {
+      case D.Messages.InOut.IN:
+        return 'in';
+      case D.Messages.InOut.OUT:
+        return 'out';
+      case D.Messages.InOut.OUTING:
+        return 'outing';
+      case D.Messages.InOut.OUTING_DELIVERED:
+        return 'outing_delivered';
+      default:
+        return '';
+    }
+  }
+});
+
 Template.channelMessages.helpers({
   messages() {
     return this.messages({sort: {timestamp: 1}});
@@ -13,5 +30,27 @@ Template.channelTitle.helpers({
   templateName() {
     let suffix = s(this.category.toLowerCase()).capitalize().value();
     return `channelTitle${suffix}`;
+  }
+});
+
+Template.channelReply.events({
+  "submit #send-message-form": function (event) {
+    event.preventDefault();
+    let form = event.target;
+    let channelId = form.channelId.value;
+    let content = form.content.value;
+
+    let doc = {
+      channelId: channelId,
+      content: content,
+      inOut: D.Messages.InOut.OUTING,
+      timestamp: moment().valueOf()
+    }
+    $(form.content).attr("disabled", "disabled");
+    D.Messages.insert(doc, function(error) {
+      // TODO: handle error
+      $(form.content).val('');
+      $(form.content).removeAttr("disabled");
+    });
   }
 });
