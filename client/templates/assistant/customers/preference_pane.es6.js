bdd0900d0aff1081a7f7c9a44a0390853c26f31d
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-06-15 18:15:23 +0800

    Moved generate invoice and settings out of the list item

--- client/templates/assistant/customers/preference_pane.es6.js
+++ client/templates/assistant/customers/preference_pane.es6.js
@@ -1,3 +1,24 @@
+Template.assistantCustomerPreferencePane.onRendered(function() {
+  let onDateRangePickerApply = function (customer, start, end, label) {
+    let invoiceRelated = {
+      customer : customer,
+      from : start.valueOf(),
+      to : end.valueOf(),
+      tasks : Tasks.find({ requestorId : customer._id })
+    };
+    Modal.show("invoice", invoiceRelated);
+  };
+
+  this.$('.export').daterangepicker(
+      {
+        format: 'YYYY-MM-DD',
+        opens: 'right',
+        maxDate: moment()
+      },
+      _.partial(onDateRangePickerApply, this.data)
+  );
+});
+
 Template.assistantCustomerPreferencePane._submitFn = (form, userId) => {
   let message = form.target.message.value;
   Customers.Preference.add(userId, message,
@@ -13,6 +34,9 @@ Template.assistantCustomerPreferencePane.helpers({
 });
 
 Template.assistantCustomerPreferencePane.events({
+  "click .profile" : function() {
+    Modal.show('customerEditForm', this);
+  },
   "submit .add-preference" : function(e) {
     e.preventDefault();
     return Template.assistantCustomerPreferencePane._submitFn(e, this._id, this.isCurrent);
