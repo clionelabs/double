2bdd067373669a779e1223a3671543568cfaf642
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-06-23 18:57:32 +0800

    Add test cases

--- tests/mocha/server/taskStepsTest.es6.js
+++ tests/mocha/server/taskStepsTest.es6.js
@@ -21,9 +21,9 @@ if (!(typeof MochaWeb === 'undefined')) {
         chai.assert.deepEqual(step.isCompleted, false);
 
       });
-      it('should toggleComplete checklist', function() {
+      it('should toggleComplete checklist true', function() {
         var stepId = Random.id();
-        _.extend(demoStep, { _id : stepId });
+        _.extend(demoStep, { _id : stepId, isCompleted : false });
         Tasks.update({ _id : taskId }, { $push : { steps : demoStep }});
 
         Tasks.Steps.toggleComplete(taskId, stepId);
@@ -31,6 +31,27 @@ if (!(typeof MochaWeb === 'undefined')) {
         task = Tasks.findOne({ _id : taskId });
         var step = _.filter(task.steps, function(step) { return _.isEqual(step._id, stepId); })[0];
         chai.assert.deepEqual(step.isCompleted, true);
+      });
+      it('should toggleComplete checklist false', function() {
+        var stepId = Random.id();
+        _.extend(demoStep, { _id : stepId, isCompleted : true });
+        Tasks.update({ _id : taskId }, { $push : { steps : demoStep }});
+
+        Tasks.Steps.toggleComplete(taskId, stepId);
+
+        task = Tasks.findOne({ _id : taskId });
+        var step = _.filter(task.steps, function(step) { return _.isEqual(step._id, stepId); })[0];
+        chai.assert.deepEqual(step.isCompleted, false);
+      });
+      it('should delete steps', function() {
+        var stepId = Random.id();
+        _.extend(demoStep, { _id : stepId });
+        Tasks.update({ _id : taskId }, { $push : { steps : demoStep }});
+
+        Tasks.Steps.delete(taskId, stepId);
+
+        task = Tasks.findOne({ _id : taskId });
+        chai.assert.deepEqual(task.steps, []);
       });
 
     });
