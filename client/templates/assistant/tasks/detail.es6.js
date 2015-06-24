030a6f10365199e4d76e9c48ba8a1df4e4e5cbe6
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-06-24 11:47:17 +0800

    Added findOneCustomer and findOneAssistant

--- client/templates/assistant/tasks/detail.es6.js
+++ client/templates/assistant/tasks/detail.es6.js
@@ -13,7 +13,7 @@ Template.assistantTasksDetail.helpers({
     return formatter(+Session.get(SessionKeys.CURRENT_TIME_USED));
   },
   getCustomerName() {
-    return Users.findCustomers({ _id : this.requestorId }).fetch()[0].displayName();
+    return Users.findOneCustomer({ _id : this.requestorId }).displayName();
   },
   last7DaysTimeUsed() {
     let weekBeforeTimestamp = moment().subtract(7, 'd').valueOf();
