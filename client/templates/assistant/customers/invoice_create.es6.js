f4b9bd3b89cdddb7d60d9bd38f06b263dc90e9b0
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-07-07 12:33:20 +0800

    Added invoice creation logic

--- client/templates/assistant/customers/invoice_create.es6.js
+++ client/templates/assistant/customers/invoice_create.es6.js
@@ -1,5 +1,4 @@
 Template.invoiceCreate.onRendered(function() {
-  console.log(Template.currentData());
   let from = Template.currentData().from;
   let to = Template.currentData().to;
   let formatter = UI._globalHelpers['formatDate'];
@@ -29,43 +28,22 @@ Template.invoiceCreate.helpers({
   }
 });
 
-Template.taskInvoiceItem.helpers({
-  getTimeSheets() {
-    let from = this.queryRange.from;
-    let to = this.queryRange.to;
-    let timesheets = _.clone(this.timesheets);
-    let statuses = _.clone(this.statuses);
-    let title = this.title;
+Template.invoiceCreate.events({
+  "click .generate-draft" : function(e, tmpl) {
+    tmpl.$('.loading').removeClass('hide');
 
-    let result = [];
+    let from = Template.currentData().from.get();
+    let to = Template.currentData().to.get();
+    let tasks = Template.currentData().tasks.fetch();
+    let customer = Template.currentData().customer;
 
-    _.each(_.keys(timesheets), (userId)=>{
-      timesheets[userId] = _.filter(timesheets[userId], (timesheet)=> {
-        return timesheet.startedAt >= from && timesheet.startedAt < to;
-      });
-      statuses[userId] = _.filter(statuses[userId], (status)=> {
-        return status.createdAt >= from && status.createdAt < to;
-      });
-
-      let iS = 0;
-      _.each(timesheets[userId], (timesheet, index, tss) => {
-        let tsUserId = userId;
-        let tsStatuses = [];
-        let endAt = tss[index + 1] ? tss[index + 1].startedAt : to;
-
-        while (statuses[userId][iS] && statuses[userId][iS].createdAt < endAt) {
-          tsStatuses.push(statuses[userId][iS]);
-          iS++;
-        }
-
-        timesheet = _.extend(timesheet, { userId : tsUserId, statuses : tsStatuses });
-      });
-      result = result.concat(timesheets[userId]);
+    let invoiceId = Invoices.insert(Invoice.convertFromTasks(from, to, tasks, customer._id), () => {
+      console.log(invoiceId);
+      tmpl.$('.loading').addClass('hide');
+      //TODO
     });
 
-    return result;
-
   }
-
 });
+
 
