a3d70ddff819f8573af90f64b73b4575a73d90b7
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-07-17 16:22:35 +0800

    Added bank task

--- tests/mocha/server/usersTest.es6.js
+++ tests/mocha/server/usersTest.es6.js
@@ -7,13 +7,14 @@ if (Meteor.isServer) {
         beforeEach(function () {
           userId = Meteor.users.insert({});
           this.clock = sinon.useFakeTimers();
+          this.clock.tick(1);
         });
 
         it("should start current work", function () {
           Customers.startTask(userId, taskId);
           chai.assert.equal(Meteor.users.findOne(userId).currentTask.taskId, taskId);
           chai.assert.equal(Meteor.users.findOne(userId).currentTask.status, Customers.TaskStatus.Started);
-          chai.assert.equal(Meteor.users.findOne(userId).currentTask.startedAt, 0);
+          chai.assert.equal(Meteor.users.findOne(userId).currentTask.startedAt, 1);
         });
         it("should end current work", function () {
           Customers.startTask(userId, taskId);
@@ -21,21 +22,42 @@ if (Meteor.isServer) {
           Customers.endTask(userId, taskId);
           chai.assert.equal(Meteor.users.findOne(userId).currentTask.taskId, taskId);
           chai.assert.equal(Meteor.users.findOne(userId).currentTask.status, Customers.TaskStatus.Stopped);
-          chai.assert.equal(Meteor.users.findOne(userId).currentTask.startedAt, 0);
-          chai.assert.equal(Meteor.users.findOne(userId).currentTask.endedAt, 10);
+          chai.assert.equal(Meteor.users.findOne(userId).currentTask.startedAt, 1);
+          chai.assert.equal(Meteor.users.findOne(userId).currentTask.endedAt, 11);
         });
         it("should not start if there is current work", function () {
           Customers.startTask(userId, taskId);
           this.clock.tick(10);
           chai.assert.throw(function() {
             Customers.startTask(userId, taskId);
-          }, Meteor.Error, 'There is running task');
+          }, Meteor.Error, 'There is running or stopped task');
         });
         it("should not end if there is no current work", function () {
           Meteor.users.update(userId, { $unset : { currentTask : "" }});
           chai.assert.throw(function() {
             Customers.endTask(userId, taskId);
           }, Meteor.Error, 'There is no running task');
+        });
+        it("should bank stopped work", function () {
+          Customers.startTask(userId, taskId);
+          this.clock.tick(10);
+          Customers.endTask(userId, taskId);
+          this.clock.tick(10);
+          console.log('user', Meteor.users.findOne(userId));
+          Customers.bankTask(userId, taskId);
+          chai.assert(!Meteor.users.findOne(userId).currentTask);
+        });
+        it("should not bank started work", function () {
+          Customers.startTask(userId, taskId);
+          this.clock.tick(10);
+          chai.assert.throw(function() {
+            Customers.bankTask(userId, taskId);
+          }, Meteor.Error, 'There is no stopped task');
+        });
+        it("should not bank no work", function () {
+          chai.assert.throw(function() {
+            Customers.bankTask(userId, taskId);
+          }, Meteor.Error, 'There is no stopped task');
         });
 
         afterEach(function () {
