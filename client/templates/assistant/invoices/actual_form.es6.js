ee9655772c8918154a6059c9b893a0aa3de79ec6
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-07-29 14:53:57 +0800

    Removed editable in invoice

--- client/templates/assistant/invoices/actual_form.es6.js
+++ client/templates/assistant/invoices/actual_form.es6.js
@@ -1,99 +1,36 @@
-let actualFormCommonHelpers = {
-  fillHeaderIfEditable() {
-    return this.isEditable() ? "tableHeaderFiller" : null;
-  },
-  showIfEditable() {
-    return this.isEditable() ? "" : "hide";
-  }
-};
-
-Template.assistantsInvoiceActualForm.helpers(_.extend({
+Template.assistantsInvoiceActualForm.helpers({
   showOneTimePurchases() {
-    return (this.oneTimePurchases && this.oneTimePurchases.length) || this.isEditable()
+    return (this.oneTimePurchases && this.oneTimePurchases.length)
               ? "assistantsInvoiceActualFormOneTimePurchasesTable" : "";
   },
   showTimeBasedItems() {
-    return (this.timeBasedItems && this.timeBasedItems.length) || this.isEditable()
+    return (this.timeBasedItems && this.timeBasedItems.length)
         ? "assistantsInvoiceActualFormTimeBasedItemsTable" : "";
   },
   debitedOrDue() {
     return this.isCustomerPaymentMethodAvailable ? 'Debited' : 'Due';
   }
-}, actualFormCommonHelpers));
-
-Template.assistantsInvoiceActualForm.events({
-  "click .add-time-based-item" : function() {
-    let invoice = this;
-    let invoiceId = invoice._id;
-    Invoice.TimeBasedItem.createEmpty(invoiceId);
-  },
-  "click .add-one-time-purchase" : function() {
-    let invoice = this;
-    let invoiceId = invoice._id;
-    Invoice.OneTimePurchase.createEmpty(invoiceId);
-  },
-  "blur input.credit" : function(e, tmpl) {
-    let invoiceId = tmpl.data._id;
-    let newCredit = moment.duration(e.currentTarget.value).valueOf();
-    Invoices.update({ _id : invoiceId }, { $set : { credit : newCredit }});
-  },
-  "blur input.effective-rate" : function(e, tmpl) {
-    let invoiceId = tmpl.data._id;
-    let newEffectiveRate = parseFloat(e.currentTarget.value);
-    Invoices.update({ _id : invoiceId }, { $set : { effectiveRate : newEffectiveRate }});
-  }
 });
 
-Template.assistantsInvoiceActualFormTimeBasedItemsTable.helpers(_.extend({
+Template.assistantsInvoiceActualFormTimeBasedItemsTable.helpers({
   timeBasedItems() {
-    let invoiceId = this._id;
-    let status = this.status;
-    let isStatic = this.isStatic;
-
-    let argTimeBasedItems = _.map(
-        this.timeBasedItems,
-        (timeBasedItem, i, timeBasedItems) => {
-          return _.extend({},
-              timeBasedItem,
-              {
-                isEditable : Invoice.ProtoType.isEditable,
-                isStatic: isStatic,
-                isEditing : new ReactiveVar(false),
-                status : status,
-                invoiceId : invoiceId
-              });
-        });
-    return _(argTimeBasedItems)
+    let timeBasedItems = this.timeBasedItems;
+    return _(timeBasedItems)
             .chain()
             .sortBy('totalDuration')
             .sortBy('updates')
             .sortBy('date')
             .sortBy('title').value();
   }
-}, actualFormCommonHelpers));
+});
 
-Template.assistantsInvoiceActualFormOneTimePurchasesTable.helpers(_.extend({
+Template.assistantsInvoiceActualFormOneTimePurchasesTable.helpers({
   oneTimePurchases() {
-    let invoiceId = this._id;
-    let status = this.status;
-    let isStatic = this.isStatic;
-    let argOneTimePurchases = _.map(
-        this.oneTimePurchases,
-          (oneTimePurchase, i, oneTimePurchases) => {
-            return _.extend({}, oneTimePurchase,
-                {
-                  isEditable : Invoice.ProtoType.isEditable,
-                  isStatic: isStatic,
-                  isEditing : new ReactiveVar(false),
-                  invoiceId : invoiceId,
-                  oneTimePurchases : oneTimePurchases,
-                  status : status
-                });
-          });
-    return _(argOneTimePurchases)
+    let oneTimePurchases = this.oneTimePurchases;
+    return _(oneTimePurchases)
         .chain()
         .sortBy('amount')
         .sortBy('title')
         .sortBy('date').value();
   }
-}, actualFormCommonHelpers));
+});
