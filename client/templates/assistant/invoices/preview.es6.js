8d989691017db09e88759901df7e77a774a1c94e
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-08-04 12:50:35 +0800

    Added remove button in draft invoice

--- client/templates/assistant/invoices/preview.es6.js
+++ client/templates/assistant/invoices/preview.es6.js
@@ -1,3 +1,9 @@
+Template.assistantInvoicePreview.helpers({
+  isDraft() {
+    return this.status === Invoice.Status.Draft;
+  }
+});
+
 Template.assistantInvoicePreview.events({
   "click .charge" : function() {
     let invoiceId = this._id;
@@ -6,5 +12,15 @@ Template.assistantInvoicePreview.events({
   },
   "click .generate" : function() {
     Router.go(Router.current().url + "?isStatic=true");
+  },
+  "click .delete" : function() {
+    let customerId = this.customerId;
+    Invoices.remove(this._id, function(e) {
+      if (!e) {
+        Router.go('assistant.customers.invoices', { customerId : customerId });
+      } else {
+        console.log(e);
+      }
+    });
   }
 });
