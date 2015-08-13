aeea7e98a48b31ff2477064d4ce83ae39e8dcd24
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-08-13 12:21:24 +0800

    Cleaned up

--- client/templates/assistant/tasks/add_step_form.jsx
+++ client/templates/assistant/tasks/add_step_form.jsx
@@ -1,54 +1,3 @@
-/*
-
-Template.assistantTasksAddStep.onCreated(function() {
-
-  _.extend(this, {
-    isStepFormShown: new ReactiveVar(false)
-  });
-});
-
-Template.assistantTasksAddStep.onRendered(function() {
-  let selfTemplate = this;
-  selfTemplate.$('.sub').on('transitionend onanimationend', function(e) {
-    console.log('file');
-    if ($(e.target).height() > 1) {
-      selfTemplate.$('.text').focus();
-    }
-  });
-  selfTemplate.$('[data-toggle="tooltip"]').tooltip();
-});
-
-Template.assistantTasksAddStep.helpers({
-  isStepFormShown() {
-    return Template.instance().isStepFormShown.get() ? "" : "not-shown";
-  }
-});
-
-Template.assistantTasksAddStep.events({
-  "click .add-step" : function(e, tmpl) {
-    tmpl.isStepFormShown.set(true);
-  },
-  "submit form.add" : function(e) {
-    e.preventDefault();
-    return _submitFn(Template.currentData(), e, this._id);
-  },
-  "keyup form.add input" : function(e, tmpl) {
-    if (e.keyCode === 27) {//esc
-      tmpl.isStepFormShown.set(false);
-    }
-  }
-});
-
-let _submitFn = (data, form, taskId) => {
-  let text = form.target.text.value;
-  Tasks.Steps.add(text, taskId,
-      () => {
-        form.target.reset();
-        data.isStepFormShown.set(false);
-      });
-};
-
- */
 AssistantTasksAddStepForm = ReactMeteor.createClass({
   getInitialState() {
     return {
@@ -68,10 +17,8 @@ AssistantTasksAddStepForm = ReactMeteor.createClass({
     this.setState({ stepTitle : e.target.value });
   },
   _submitFn() {
-    //Block-scoped declarations (let, const, function, class) not yet supported outside strict mode
-    'use strict';
-    let text = this.state.stepTitle;
-    let taskId = this.props.taskId;
+    var text = this.state.stepTitle;
+    var taskId = this.props.taskId;
     Tasks.Steps.add(text, taskId,
         (function() {
           this.toggle();
