2d08f53451cc11e89f240b101f592088ae38d2de
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-08-13 18:14:43 +0800

    Closes #274

--- client/templates/assistant/tasks/tagged_messages.es6.js
+++ client/templates/assistant/tasks/tagged_messages.es6.js
@@ -5,3 +5,10 @@ Template.assistantTaskTaggedMessages.onRendered(function() {
     instance.subscribe('taskTaggedMessages', task._id);
   });
 });
+
+Template.assistantTaskTaggedMessages.events({
+  "click .add" : function() {
+    let task = Template.currentData();
+    Router.go('assistant.customers.selected', { _id : task.requestorId });
+  }
+});
