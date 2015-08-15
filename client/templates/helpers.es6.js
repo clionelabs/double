8f51f1a18cf6f2eb55b9555ea294e4049f2bd0b3
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-08-15 09:19:27 +0800

    Added edit action

--- client/templates/helpers.es6.js
+++ client/templates/helpers.es6.js
@@ -1,29 +1,41 @@
-Template.registerHelper("formatDate", function(date) {
-  return date ? moment(date).format('YYYY-MM-DD') : '---';
-});
+DateFormatter = {
+  toDateString : (date) => {
+    return date ? moment(date).format('YYYY-MM-DD') : '---';
+  },
+  toDateTimeString : (date) => {
+    return  date ? moment(date).format('YYYY-MM-DD HH:mm:ss') : '---';
+  },
+  toDateMonthString : (date) => {
+    return  date ? moment(date).format('MMMM DD, YYYY') : '---';
+  }
+};
 
-Template.registerHelper("formatDateTime", function(date) {
-  return  date ? moment(date).format('YYYY-MM-DD HH:mm:ss') : '---';
-});
+DurationFormatter = {
+  toString : (duration)=> {
+    return duration ? moment.duration(duration).humanize() : '---';
+  },
+  toPreciseString : (duration) => {
+    return moment.duration(duration).format('hh:mm:ss', { trim : false });
+  },
+  toPreciseMsString : function(duration) {
+    return moment.duration(duration).format('hh:mm:ss.SSS', { trim : false });
+  }
+};
 
-Template.registerHelper("formatDateMonth", function(date) {
-  return  date ? moment(date).format('MMMM DD, YYYY') : '---';
-});
+AmountFormatter = {
+  toString : function(amount) {
+    return numeral(amount).format('0,0.00');
+  }
+};
 
-Template.registerHelper("formatDuration", function(duration) {
-  return duration ? moment.duration(duration).humanize() : '---';
-});
+Template.registerHelper("formatDate", DateFormatter.toDateString);
+Template.registerHelper("formatDateTime", DateFormatter.toDateTimeString);
+Template.registerHelper("formatDateMonth", DateFormatter.toDateMonthString);
+Template.registerHelper("formatDuration", DurationFormatter.toString);
+Template.registerHelper("formatDurationPrecise", DurationFormatter.toPreciseString);
+Template.registerHelper("formatDurationPreciseMs", DurationFormatter.toPreciseMsString);
 
-Template.registerHelper("formatDurationPrecise", function(duration) {
-  return moment.duration(duration).format('hh:mm:ss', { trim : false });
-});
-Template.registerHelper("formatDurationPreciseMs", function(duration) {
-  return moment.duration(duration).format('hh:mm:ss SSS', { trim : false });
-});
-
-Template.registerHelper("formatAmount", function(amount) {
-  return numeral(amount).format('0,0.00');
-});
+Template.registerHelper("formatAmount", AmountFormatter.toString);
 
 Template.registerHelper('enableIfIsEditing', function(isEditing) {
   return isEditing.get() ? "false" : "true";
