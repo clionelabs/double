6b1b39d2a51184b5632a3d3fc8b6ca138f02aa3b
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2015-10-07 17:01:44 +0800

    Removed unused data to send to mixpanel

--- client/lib/analytics.es6.js
+++ client/lib/analytics.es6.js
@@ -1,41 +1,26 @@
 Analytics = {
-  bankTimeInMinutes(task, assistantId, durationMoment, reason) {
+  bankTimeInMinutes(task, assistantId, durationMoment) {
     let assistant = Users.findOne(assistantId);
     let customer = Users.findOneCustomer(task.requestorId);
-    analytics.identify(task.requestorId, {
-      email: customer.emails[0].address,
-      name: customer.displayName()
-    });
+    analytics.identify(task.requestorId);
     let properties = {
       byAssistantId: assistantId,
       taskTitle : task.title,
       minutesAdded : Math.ceil(durationMoment.valueOf() / 1000 / 60)
     };
-    if (reason) {
-      _.extend(properties, { reason : reason });
-    }
     analytics.track('Bank Time', properties);
-    analytics.identify(assistantId, {
-      email: assistant.emails[0].address,
-      name: assistant.displayName()
-    });
+    analytics.identify(assistantId);
   },
   createRequest(task, assistantId) {
     let assistant = Users.findOne(assistantId);
     let customer = Users.findOneCustomer(task.requestorId);
-    analytics.identify(task.requestorId, {
-      email: customer.emails[0].address,
-      name: customer.displayName()
-    });
+    analytics.identify(task.requestorId);
     let properties = {
       taskTitle : task.title,
       byAssistantId : task.responderId
     };
     analytics.track('Create Request', properties);
-    analytics.identify(assistantId, {
-      email: assistant.emails[0].address,
-      name: assistant.displayName()
-    });
+    analytics.identify(assistantId);
   }
 
 };
