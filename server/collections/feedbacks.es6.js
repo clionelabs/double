81723353660bd7fe6b891b716a827f552120930c
Thomas Pun <thomas@pun.io>
2015-11-10 11:51:23 +0800

    slacklog when feedback email is sent

--- server/collections/feedbacks.es6.js
+++ server/collections/feedbacks.es6.js
@@ -22,6 +22,26 @@ Cary & Thomas
 Feedbacks.find({ isSent : false }).observe({
   added(feedback) {
     feedback.sendEmail();
-    Feedbacks.update(feedback._id, { $set : { 'isSent' : true , sendAt : moment().valueOf() }});
+    Feedbacks.logEmailSent(feedback._id);
   }
 });
+
+Feedbacks.logEmailSent = (feedbackId) => {
+  // TODO: We should log the email address we sent to because customer can change email at anytime.
+  Feedbacks.update(feedbackId, { $set : { 'isSent' : true , sendAt : moment().valueOf() }});
+
+  const feedback = Feedbacks.findOne({_id: feedbackId});
+  const task = Tasks.findOne(feedback.taskId);
+  const customer = Users.findOneCustomer(feedback.customerId);
+  const emailTo = customer.emails[0].address;
+  const url = Router.routes['assistant.tasks'].url({ _id : task._id }); // TODO: refactor
+  SlackLog.log('_completed_requests',
+    {
+      text : `
+<!channel>, Feedback email is sent to ${emailTo} for ${task.title} (${url})` ,
+      username: 'Double A.I. Parts',
+      unfurl_links: true,
+      icon_emoji: ':robot_face:'
+    });
+
+}
