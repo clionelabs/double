5cfc8c1425452a2071bf0ffc3fffc5261adc5578
hiukim <hiukim@gmail.com>
2015-11-16 17:36:41 +0800

    slacklog tasks creation

--- server/collections/tasks.es6.js
+++ server/collections/tasks.es6.js
@@ -1,4 +1,18 @@
+let initial = true;
+
 Tasks.find({ completedAt : null }).observe({
+  added(task) {
+    if (initial) return; // ignore initial
+
+    const customer = Users.findOneCustomer({ _id : task.requestorId });
+    const url = Router.routes['assistant.tasks'].url({ _id : task._id });
+    SlackLog.log('_requests', {
+      text : `\'${task.title}\' has been created for ${customer.displayName()}. ${url}`,
+      username: 'Double A.I. Parts',
+      unfurl_links: true,
+      icon_emoji: ':robot_face:'
+    });
+  },
   changed(newTask, oldTask) {
     if (newTask.lastModified === oldTask.lastModified) {
       Tasks.update({_id: newTask._id}, {$set: {lastModified: moment().valueOf()}});
@@ -9,7 +23,7 @@ Tasks.find({ completedAt : null }).observe({
     if (newTask && newTask.completedAt && !oldTask.completedAt) {
       const url = Router.routes['assistant.tasks'].url({ _id : newTask._id });
       const customer = Users.findOneCustomer({ _id : newTask.requestorId });
-      SlackLog.log('_completed_requests',
+      SlackLog.log('_requests',
         {
           text : `\'${newTask.title}\' has been completed for ${customer.displayName()} in ${newTask.totalDurationInMinutes()} minutes. ${url}` ,
           username: 'Double A.I. Parts',
@@ -19,3 +33,5 @@ Tasks.find({ completedAt : null }).observe({
     }
   }
 });
+
+initial = false;
