fda29ccf8578ab7424a8661e1b90ce0bb0460f50
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2015-11-26 15:50:28 +0800

    Fixed some UI error when data is not completely synced

--- client/templates/assistant/tasks/dashboard.es6.js
+++ client/templates/assistant/tasks/dashboard.es6.js
@@ -43,6 +43,7 @@ Template.assistantTasksDashboard.helpers({
   tasksWithIsCurrent() {
     let tasks = Template.instance().filter.get() === FILTER.CURRENT ? this.tasks : this.completedTasks;
     let currentTask = Template.currentData().currentTask;
+    if (!(currentTask && tasks)) return [];
     return _.sortBy(_.map(tasks.fetch(), function(task) {
       return _.extend({}, { isCurrent : (currentTask._id === task._id) }, task);
     }), function(task) {
