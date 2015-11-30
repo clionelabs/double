317e9714892733bb873c37223d6329ac01606bd7
hiukim <hiukim@gmail.com>
2015-11-30 17:22:13 +0800

    tasks tab performance optimization

--- client/templates/assistant/tasks/dashboard.es6.js
+++ client/templates/assistant/tasks/dashboard.es6.js
@@ -1,65 +1,88 @@
 let FILTER = {
-  CURRENT : 'current',
+  INPROGRESS : 'inprogress',
   COMPLETED : 'completed'
 };
 
 Template.assistantTasksDashboard.onCreated(function() {
-  Template.instance().filter = new ReactiveVar(FILTER.CURRENT);
-  Template.instance().rCurrentTaskId = new ReactiveVar();
-});
+  let instance = this;
+  instance.rSubscriptionReady = new ReactiveVar(false);
+  instance.rFilter = new ReactiveVar(FILTER.INPROGRESS);
+  instance.rCurrentTask = new ReactiveVar();
+  instance.rCurrentTaskId = new ReactiveVar(); // TODO: remove this; rCurrentTask should be enough
 
-Template.assistantTasksDashboard.onRendered(function() {
   this.autorun(function() {
-    const currentTask = Template.currentData().currentTask;
-    if (currentTask) {
-      Template.instance().rCurrentTaskId.set(currentTask._id);
+    let data = Template.currentData();
+    let currentTaskId = data.currentTaskId;
+    if (currentTaskId) {
+      instance.subscribe("tasks", {_id: currentTaskId});
+      instance.rCurrentTask.set(Tasks.findOne(currentTaskId));
+      instance.rCurrentTaskId.set(currentTaskId);
+    } else {
+      instance.rCurrentTask.set(null);
+      instance.rCurrentTaskId.set(null);
     }
   })
 });
 
 Template.assistantTasksDashboard.events({
-  "click .filters>.current" : function() {
-    Template.instance().filter.set(FILTER.CURRENT);
+  "click .filters>.inprogress" : function() {
+    Template.instance().rFilter.set(FILTER.INPROGRESS);
   },
   "click .filters>.completed" : function() {
-    Template.instance().filter.set(FILTER.COMPLETED);
+    Template.instance().rFilter.set(FILTER.COMPLETED);
   }
 });
 
 Template.assistantTasksDashboard.helpers({
-  currentNumberOfTasks() {
-    return this.tasks.count();
+  isReady() {
+    return Template.instance().subscriptionsReady();
+  },
+  inProgressTasks() {
+    return Tasks.find({completedAt: null});
+  },
+  completedTasks() {
+    return Tasks.find({completedAt: {$not: null}});
+  },
+  inProgressTasksCount() {
+    let inProgressTasks = Template.assistantTasksDashboard.__helpers.get('inProgressTasks')();
+    return inProgressTasks.count();
+  },
+  completedTasksCount() {
+    let inProgressTasks = Template.assistantTasksDashboard.__helpers.get('completedTasks')();
+    return inProgressTasks.count();
   },
   getRCurrentTaskId() {
     return Template.instance().rCurrentTaskId;
   },
   getCurrentTask() {
-    let currentTask = this.currentTask;
+    let currentTask = Template.instance().rCurrentTask.get();
+
+    // TODO: following reactive vars should be created in the taskDetail sub-template?!
     return _.extend({}, {
-            isDescriptionFormShown : new ReactiveVar(false),
-            isLinkFormShown: new ReactiveVar(false),
-            isStepFormShown : new ReactiveVar(false)
-          },
-        currentTask);
+      isDescriptionFormShown : new ReactiveVar(false),
+      isLinkFormShown: new ReactiveVar(false),
+      isStepFormShown : new ReactiveVar(false)
+    }, currentTask);
   },
   tasksWithIsCurrent() {
-    let tasks = Template.instance().filter.get() === FILTER.CURRENT ? this.tasks : this.completedTasks;
-    let currentTask = Template.currentData().currentTask;
-    if (!(currentTask && tasks)) return [];
-    return _.sortBy(_.map(tasks.fetch(), function(task) {
-      return _.extend({}, { isCurrent : (currentTask._id === task._id) }, task);
-    }), function(task) {
-      if (task.completedAt) {
-        return task.completedAt * -1;
-      } else {
-        return task.lastModified * -1;
-      }
+    let currentTask = Template.instance().rCurrentTask.get();
+    let cursor;
+    if (Template.instance().rFilter.get() === FILTER.INPROGRESS) {
+      cursor =  Tasks.find({completedAt: null}, {sort: {lastModified: -1}});
+    } else {
+      cursor =  Tasks.find({completedAt: {$not: null}}, {sort: {completedAt: -1}});
+    }
+    return cursor.map(function(task) {
+      return _.extend(task, {
+        isCurrent: (currentTask && currentTask._id === task._id)
+      });
     });
+    return cursor;
   },
-  filterIsCurrent() {
-    return Template.instance().filter.get() === FILTER.CURRENT ? "active" : "";
+  filterIsInprogress() {
+    return Template.instance().rFilter.get() === FILTER.INPROGRESS ? "active" : "";
   },
   filterIsCompleted() {
-    return Template.instance().filter.get() === FILTER.COMPLETED ? "active" : "";
+    return Template.instance().rFilter.get() === FILTER.COMPLETED ? "active" : "";
   }
 });
